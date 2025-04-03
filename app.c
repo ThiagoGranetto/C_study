67ec8ccbde1df8ceda3a71dcf4c6daa25e459e56
Thiago Granetto Floriani <granettot@gmail.com>
2025-04-02 21:29:31 -0300

    switch case

--- app.c
+++ app.c
@@ -8,7 +8,7 @@ void triangulo()
     printf("Digite o primeiro lado: ");
     float lado1;
     scanf("%f", &lado1);
-    
+
     printf("Digite o segundo lado: ");
     float lado2;
     scanf("%f", &lado2);
@@ -38,14 +38,14 @@ void media()
     float nota1;
     scanf("%f", &nota1);
 
-    printf("Digite a segunda nota: ");  
+    printf("Digite a segunda nota: ");
     float nota2;
     scanf("%f", &nota2);
 
     printf("Digite a terceira nota: ");
     float nota3;
     scanf("%f", &nota3);
-    
+
     float media = (nota1 + nota2 + nota3) / 3;
     printf("A média da turma é: %.2f\n", media);
     return 0;
@@ -69,7 +69,7 @@ void parimpar()
     printf("Digite um número:");
     int num;
     scanf("%i", &num);
-    if(num % 2 == 0)
+    if (num % 2 == 0)
     {
         printf("O número é par\n");
     }
@@ -77,14 +77,14 @@ void parimpar()
     {
         printf("O número é ímpar\n");
     }
-    if(num % 5 == 0)
+    if (num % 5 == 0)
     {
         printf("O número é múltiplo de 5\n");
     }
     else
     {
         printf("O número não é múltiplo de 5\n");
-    } 
+    }
 }
 
 void anobisexto()
@@ -92,11 +92,11 @@ void anobisexto()
     printf("Digite um ano:");
     int ano;
     scanf("%i", &ano);
-    if(ano % 4 == 0 && ano % 100 != 0)
+    if (ano % 4 == 0 && ano % 100 != 0)
     {
         printf("O ano é bisexto\n");
     }
-    else if(ano % 4 == 0 && ano % 100 == 0 && ano % 400 == 0)
+    else if (ano % 4 == 0 && ano % 100 == 0 && ano % 400 == 0)
     {
         printf("O ano é bisexto\n");
     }
@@ -107,40 +107,37 @@ void anobisexto()
 }
 
 int main()
-{   
+{
     int opcao;
-    do{
-    printf("Digite a função a ser selecionada\n");
-    printf("1-Função Triangulo\n");
-    printf("2-Função Media\n");
-    printf("3-Par ou Impar\n");
-    printf("4-Ano Bisexto\n");
-    printf("0-Sair\n");
-    scanf("%d", &opcao);
-    if(opcao == 1){
-        triangulo();
-    }
-    else if(opcao == 2){
-        media();
-    }
-    else if(opcao == 3){
-        parimpar();
-        
-    }
-    else if(opcao == 4){
-        anobisexto();
-    }
-    else if(opcao == 0){
-        exit;
-    }
-    else{
-        printf("Opção inválida\n");
-    } 
-    return 0;
-
-    }while(opcao != 0);
-    printf("Saindo do programa\n");   
-    printf("Obrigado por usar o programa\n");
-    
-    return 0;
+        printf("Digite a função a ser selecionada\n");
+        printf("1-Função Triangulo\n");
+        printf("2-Função Media\n");
+        printf("3-Par ou Impar\n");
+        printf("4-Ano Bisexto\n");
+        printf("0-Sair\n");
+        scanf("%d", &opcao);
+        switch (opcao)
+        {
+            case 1:
+                triangulo();
+                break;
+            case 2:
+                media();
+                break;
+            case 3:
+                parimpar();
+                break;
+            case 4:
+                anobisexto();
+                break;
+            case 0:
+                printf("Saindo do programa\n");
+                printf("Obrigado por usar o programa\n");
+                printf("Volte sempre\n");
+                break;
+            default:
+                printf("Opção inválida\n");
+                break;
+            }      
+            return 0;
 }
